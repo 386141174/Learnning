f96ba8e56383bcebec037ecf196c93aa6e0d5118
Administrator <386141174@qq.com>
2020-10-25 21:40:13 +0800

    discovery

--- provide/src/main/java/com/springcloud/provide/controller/ProvideController.java
+++ provide/src/main/java/com/springcloud/provide/controller/ProvideController.java
@@ -1,14 +1,13 @@
 package com.springcloud.provide.controller;
 
 
-import com.springcloud.common.TestCommon;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
 
 @RestController
 public class ProvideController {
--- springconsumer/src/main/java/com/springcloud/consumer/controller/TestConsumer.java
+++ springconsumer/src/main/java/com/springcloud/consumer/controller/TestConsumer.java
@@ -2,12 +2,15 @@ package com.springcloud.consumer.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 
 @RestController
@@ -16,6 +19,8 @@ public class TestConsumer {
 
     private final String TEST_CONSUMER = "http://SPRINGCLOUD-PROVIDER";
 
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Resource
     private RestTemplate restTemplate;
@@ -25,5 +30,22 @@ public class TestConsumer {
 //        RestTemplate restTemplate = new RestTemplate();
         String respond = restTemplate.getForObject(TEST_CONSUMER+"/testProvider",String.class);
         return "aaaaaa";
+    }
+
+    @GetMapping("/discovery/consumer")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String serviceName:services
+             ) {
+            System.out.println("************* service" + serviceName);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("springCloud-provider");
+        for (ServiceInstance id:instances
+             ) {
+            System.out.println("************** id" + id.getServiceId() + "\t" +id.getHost()+ "\t"  + id.getPort()  + "\t" +id.getUri() + "\t" + id.getInstanceId());
+        }
+
+        return this.discoveryClient;
     }
 }
